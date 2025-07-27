5045d870ae111aad8c252bc3032024fa6bbc0985
anurag <anuragkumar78702685001@gmail.com>
2025-07-27 18:38:43 +0000

    Code challenge submission for question: 1346 Check If N and Its Double Exist

---
+++ 1346 Check If N and Its Double Exist/1346check-if-n-and-its-double-exist.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(2*arr[i]==arr[j]){
+                    return true;
+                }
+                else if(arr[i]==2*arr[j]){
+                    return true;
+                }
+            }
+        }
+       return false; 
+    }
+}
