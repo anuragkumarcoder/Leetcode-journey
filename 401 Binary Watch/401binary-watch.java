0f3050ca1336ad46374b067d0d33e0cca7d92758
anurag <anuragkumar78702685001@gmail.com>
2025-08-16 05:16:22 +0000

    Code challenge submission for question: 401 Binary Watch

---
+++ 401 Binary Watch/401binary-watch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans=new ArrayList<>();
+        for(int hour=0;hour<12;hour++){
+            for(int minute=0;minute<60;minute++){
+                if(Integer.bitCount(hour)+Integer.bitCount(minute)==turnedOn){
+                    if(minute>=10){
+                    ans.add(hour+":"+minute);
+                    }
+                    else{
+                        ans.add(hour+":0"+minute);
+                    }
+                }
+
+                
+            }
+        }
+        return ans;
+    }
+}
