2d0dd845983ae19aa7a694fb0326f9c609b31652
anurag <anuragkumar78702685001@gmail.com>
2025-08-19 18:46:58 +0000

    Code challenge submission for question: 2433 Find The Original Array of Prefix Xor

---
+++ 2433 Find The Original Array of Prefix Xor/2433find-the-original-array-of-prefix-xor.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] findArray(int[] pref) {
+        
+        int[] arr=new int[pref.length];
+        if(pref.length==0){
+            return arr;
+        }
+        arr[0]=pref[0];
+        for(int i=1;i<pref.length;i++){
+            
+            arr[i]=pref[i]^pref[i-1];
+        }
+        return arr;
+    }
+
+}
