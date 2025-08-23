1ce16486f3ceca198b141fd9d0d1e4f685150fe1
anurag <anuragkumar78702685001@gmail.com>
2025-08-23 06:51:42 +0000

    Code challenge submission for question: 1616 Split Two Strings to Make Palindrome

---
+++ 1616 Split Two Strings to Make Palindrome/1616split-two-strings-to-make-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkPalindromeFormation(String a, String b) {
+        return check(a, b) || check(b, a);
+    }
+
+    private boolean check(String a, String b) {
+        int i = 0, j = a.length() - 1;
+        while (i < j && a.charAt(i) == b.charAt(j)) {
+            i++;
+            j--;
+        }
+        return isPalindrome(a, i, j) || isPalindrome(b, i, j);
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
