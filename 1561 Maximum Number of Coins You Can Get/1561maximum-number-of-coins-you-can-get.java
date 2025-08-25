d474e73f535606854f13dd94edd51cab27b7037a
anurag <anuragkumar78702685001@gmail.com>
2025-08-25 08:45:13 +0000

    Code challenge submission for question: 1561 Maximum Number of Coins You Can Get

---
+++ 1561 Maximum Number of Coins You Can Get/1561maximum-number-of-coins-you-can-get.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int maxCoins(int[] piles) {
+        int ans=0;
+        int n=piles.length;
+        Arrays.sort(piles);
+        for(int i=n-2;i>=n/3;i-=2){
+            ans+=piles[i];
+        }
+       
+        return ans;
+    }
+   
+}
