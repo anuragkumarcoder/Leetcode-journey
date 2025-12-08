d0f8f192e336224e453986120a70c08e7ff8e33b
anurag <anuragkumar78702685001@gmail.com>
2025-12-08 06:21:11 +0000

    Code challenge submission for question: 865 Smallest Subtree with all the Deepest Nodes

---
+++ 865 Smallest Subtree with all the Deepest Nodes/865smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution { class Pair {
+        TreeNode node;
+        int depth;
+        Pair(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+    
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return helper(root).node;
+    }
+    private Pair helper(TreeNode root) {
+        if (root == null) {
+            return new Pair(null, 0);
+        }
+        Pair left = helper(root.left);
+        Pair right = helper(root.right);
+        if (left.depth == right.depth) {
+            return new Pair(root, left.depth + 1);
+        }
+        if (left.depth > right.depth) {
+            return new Pair(left.node, left.depth + 1);
+        }
+        return new Pair(right.node, right.depth + 1);
+    }
+}
