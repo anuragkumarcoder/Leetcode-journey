4c05b8eccc93b8d7744e18cdc63bc669e8ea90c2
anurag <anuragkumar78702685001@gmail.com>
2025-12-28 09:40:18 +0000

    Code challenge submission for question: 1748 Sum of Unique Elements

---
+++ 1748 Sum of Unique Elements/1748sum-of-unique-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int x: nums){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+        int sum=0;
+        for(int x: map.keySet()){
+            if(map.get(x)==1){
+                sum+=x;
+            }
+        }
+        return sum;
+    }
+}
