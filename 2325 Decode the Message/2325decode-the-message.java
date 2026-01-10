9607da8c6ea1903412cf082e6a6c32e318f8d845
anurag <anuragkumar78702685001@gmail.com>
2026-01-10 12:35:40 +0000

    Code challenge submission for question: 2325 Decode the Message

---
+++ 2325 Decode the Message/2325decode-the-message.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        HashMap<Character, Character> map = new HashMap<>();
+        int ind = 97;
+        for (char c : key.toCharArray()) {
+            if (c == ' ') {
+                continue;
+            } if(!map.containsKey(c)) {
+                map.put(c, (char) ind++);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            if (c == ' ') {
+                sb.append(' ');
+            }
+            else{
+                sb.append(map.get(c));
+            }
+        }
+
+        return sb.toString();
+    }
+}
