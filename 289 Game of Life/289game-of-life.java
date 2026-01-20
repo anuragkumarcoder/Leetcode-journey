2185d506cc3a80fe602646c93ab142ef90e10453
anurag <anuragkumar78702685001@gmail.com>
2026-01-20 11:31:57 +0000

    Code challenge submission for question: 289 Game of Life

---
+++ 289 Game of Life/289game-of-life.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int[][] copyBoard = new int[board.length][board[0].length];
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                copyBoard[i][j] = board[i][j];
+            }
+        }
+        for(int i=0;i<board.length;i++){
+            
+            for(int j=0;j<board[i].length;j++){
+                int live=0;
+                if(i+1<board.length && copyBoard[i+1][j]==1){
+                    live++;
+                    
+                }
+                if(i-1>=0 && copyBoard[i-1][j]==1){
+                    live++;
+                }
+                if(j+1<board[i].length && copyBoard[i][j+1]==1){
+                    live++;
+                }
+                if(j-1>=0 && copyBoard[i][j-1]==1){
+                    live++;
+                }
+                if(i+1<board.length && j+1<board[i].length && copyBoard[i+1][j+1]==1){
+                    live++;
+                }
+                if(i+1<board.length && j-1>=0 && copyBoard[i+1][j-1]==1){
+                    live++;
+                }
+                if(i-1>=0 && j-1>=0 && copyBoard[i-1][j-1]==1){
+                    live++;
+                      
+                }
+                if(i-1>=0 && j+1<board[i].length && copyBoard[i-1][j+1]==1){
+                    live++;
+                }
+                if(copyBoard[i][j] == 1) {
+                    if(live < 2 || live > 3) {
+                        board[i][j] = 0; 
+                    }
+                } 
+                else {
+                    if(live == 3) {
+                        board[i][j] = 1;
+                    }    
+                }    
+
+            }
+        }
+    }
+}
